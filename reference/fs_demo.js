56de8c66446e3c6e4b6173397d27632f1c002fc4
Mert Demirok <mertdemirok80@gmail.com>
2019-12-16 18:38:10 +0100

    Create and write to a file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,7 +2,18 @@ const fs = require("fs");
 const path = require("path");
 
 //Create folder
-fs.mkdir(path.join(__dirname, '/test'), {}, err => { //by default "fs.mkdir" is async
+fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+  //by default "fs.mkdir" is async
   if (err) throw err;
   console.log("Folder created...");
 });
+
+//Create and write to a file
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "Hello World!",
+  err => {
+    if (err) throw err;
+    console.log("File written to...");
+  }
+);
