2f1cfa7178b78688a3290bed22e13aa39b4e4647
Mert Demirok <mertdemirok80@gmail.com>
2019-12-16 20:17:34 +0100

    rename a file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -33,3 +33,9 @@ fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf-8", (err, data) => 
   if (err) throw err;
   console.log(data);
 });
+
+// Rename file
+fs.rename(path.join(__dirname, "/test", "hello.txt"), path.join(__dirname, "/test", "helloWorld.txt"), err => {
+    if (err) throw err;
+    console.log("File renamed...");
+  });
