b12c0873559ab5953f0cda416d4f8054f361cea3
Mert Demirok <mertdemirok80@gmail.com>
2019-12-16 20:13:30 +0100

    read a file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -9,20 +9,27 @@ const path = require("path");
 // });
 
 //Create and write to a file
-fs.writeFile(
-  path.join(__dirname, "/test", "hello.txt"),
-  "Hello World!",
-  err => {
-    if (err) throw err;
-    console.log("File written to...");
-    // Append a file
-    fs.appendFile( // this is a callback function
-      path.join(__dirname, "/test", "hello.txt"),
-      " I love Node JS",
-      err => {
-        if (err) throw err;
-        console.log("File written to...");
-      }
-    );
-  }
-);
+// fs.writeFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   "Hello World!",
+//   err => {
+//     if (err) throw err;
+//     console.log("File written to...");
+
+//     // Append a file
+//     fs.appendFile( // this is a callback function
+//       path.join(__dirname, "/test", "hello.txt"),
+//       " I love Node JS",
+//       err => {
+//         if (err) throw err;
+//         console.log("File written to...");
+//       }
+//     );
+//   }
+// );
+
+// Read file
+fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
